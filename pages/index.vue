7c8ea937884a94a4dfbea46d8c4a7d30277f9223
Nikita Mihaylov <michailovn14@gmail.com>
2024-11-30 21:11:24 +0300

    Fix tauri folder problem

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import { ask } from '@tauri-apps/plugin-dialog';
+// when using `"withGlobalTauri": true`, you may use
+// const { ask } = window.__TAURI__.dialog;
+
+const askFunc = async () => {
+// Create a Yes/No dialog
+const answer = await ask('This action cannot be reverted. Are you sure?', {
+  title: 'Tauri',
+  kind: 'warning',
+});
+console.log(answer);
+}
+
+</script>
+
+<template>
+<button @click="askFunc">Click me</button>
+</template>
