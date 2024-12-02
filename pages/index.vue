9763e0adfca6e0cdd0cf02e5dadfb555033d886d
Nikita Mihaylov <michailovn14@gmail.com>
2024-12-02 16:24:29 +0300

    Add updater on fixed layout, regenerated key/pwd signing

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
+// sample front-end code for the updater
+import { check } from '@tauri-apps/plugin-updater';
+import { ask, message } from '@tauri-apps/plugin-dialog';
 
-import { ask } from '@tauri-apps/plugin-dialog';
-// when using `"withGlobalTauri": true`, you may use
-// const { ask } = window.__TAURI__.dialog;
-
-const askFunc = async () => {
-// Create a Yes/No dialog
-const answer = await ask('This action cannot be reverted. Are you sure?', {
-  title: 'Tauri',
-  kind: 'warning',
-});
-console.log(answer);
+async function checkForAppUpdates(onUserClick: boolean) {
+  const update = await check();
+  if (update === null) {
+			await message('Failed to check for updates.\nPlease try again later.', { 
+				title: 'Error',
+				kind: 'error',
+				okLabel: 'OK'
+			});
+			return;
+		} else if (update?.available) {
+    const yes = await ask(`Update to ${update.version} is available!\n\nRelease notes: ${update.body}`, { 
+      title: 'Update Available',
+      kind: 'info',
+      okLabel: 'Update',
+      cancelLabel: 'Cancel'
+    });
+    if (yes) {
+      await update.downloadAndInstall();
+    }
+  } else if (onUserClick) {
+    await message('You are on the latest version. Stay awesome!', { 
+      title: 'No Update Available',
+      kind: 'info',
+      okLabel: 'OK'
+    });
+  }
 }
-
 </script>
 
 <template>
-<button @click="askFunc">Click me</button>
+<button @click="checkForAppUpdates(true)">Click me</button>
 </template>
